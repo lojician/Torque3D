c2298beb8d33cfe2743062516914e348f287dcf3
lojician <illjot@gmail.com>
2021-01-21 14:12:36 -0500

    implementing game class

--- Game.cpp
+++ Game.cpp
@@ -3,13 +3,13 @@ Game::Game(){
     board = new Board(board_size);
 }
 Game::~Game(){
-
+    delete board;
 }
 void Game::Start(){
-
+    game_status = started;
 }
 void Game::End(){
-
+    game_status = unstarted;
 }
 void Game::CalculateScore(){
 
